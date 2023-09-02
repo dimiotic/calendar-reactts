c5fb15ba7a2a452911f914e8a6474096c460e31c
dimiotic <dimiotic@gmail.com>
2023-09-02 14:38:43 +0200

    stage1

--- src/components/Calendar/CalendarComponent.tsx
+++ src/components/Calendar/CalendarComponent.tsx
@@ -1,30 +1,134 @@
-import React from 'react';
-import CalendarDates from './CalendarDates';
+import React, { useState } from 'react';
 import moment from 'moment';
-import CalendarHeader from './CalendarHeader';
+import { styled } from 'styled-components';
+import DateItem from './DateItem';
 
 const CalendarComponent = () => {
-  moment.updateLocale('en', {
+  const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const [today, setToday] = useState(moment());
+  moment.updateLocale('eu', {
     week: {
       dow: 1,
     },
   });
-  const startDate = moment().clone().startOf('month').startOf('week');
-  // const endDate = moment().clone().endOf('month').endOf('week');
+  const startDate = today.clone().startOf('month').startOf('week');
+  const day = startDate.clone().subtract(1, 'day');
+  const daysOfMonth = [...Array(42)].map(() => day.add(1, 'day').clone());
+  const thisMonth = today.clone().add(1, 'week').format('MMMM ');
+  const setPrevMonth = () => {
+    setToday((prev) => prev.clone().subtract(1, 'month'));
+  };
+  const setThisMonth = () => {
+    setToday(moment());
+  };
+  const setNextMonth = () => {
+    setToday((prev) => prev.clone().add(1, 'month'));
+  };
 
-  // const daysOfMonth = [];
-  // const day = startDate;
-  // while (!day.isAfter(endDate)) {
-  //   daysOfMonth.push(day.clone());
-  //   day.add(1, 'day');
-  // }
-  console.log(typeof startDate);
   return (
     <main>
-      <CalendarHeader />
-      <CalendarDates startDate={startDate} />
+      <HeaderWrapper>
+        <HeaderTopRow>
+          <section>
+            <h1>{thisMonth}</h1>
+            <p>{startDate.clone().add(1, 'week').format('Y')}</p>
+          </section>
+          <ButtonControlWrapper>
+            <button onClick={setPrevMonth}>&#60;</button>
+            <button className="todayBtn" onClick={setThisMonth}>
+              Today
+            </button>
+            <button onClick={setNextMonth}>&#62;</button>
+          </ButtonControlWrapper>
+        </HeaderTopRow>
+        <DaysOfWeekWrapper>
+          {daysOfWeek.map((item) => (
+            <DayOfWeek
+              $weekendDay={item === 'Sat' || item === 'Sun'}
+              key={item}
+            >
+              {item}
+            </DayOfWeek>
+          ))}
+        </DaysOfWeekWrapper>
+      </HeaderWrapper>
+      <DatesWrapper>
+        {daysOfMonth.map((dayItem) => {
+          return (
+            <DateItem
+              thisMonth={thisMonth}
+              date={dayItem.format('D MMM Y ddd')}
+              key={dayItem.format('D M Y')}
+            />
+          );
+        })}
+      </DatesWrapper>
     </main>
   );
 };
+const HeaderWrapper = styled.section`
+  background-color: #1f1e1e;
+  section {
+    display: flex;
+  }
+  h1 {
+    font-size: 35px;
+    color: white;
+    padding: 10px;
+  }
+  p {
+    font-size: 35px;
+    font-weight: 400;
+    padding: 10px 0;
+    color: white;
+  }
+`;
+const ButtonControlWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 30px;
+  gap: 1px;
+  button {
+    padding: 3px 7px;
+    border: 0;
+    background-color: #555555;
+    color: white;
+    border-radius: 5px;
+  }
+  button:hover {
+    cursor: pointer;
+    background-color: #4d4d4d;
+  }
+  button:active {
+    background-color: #444444;
+  }
+  .todayBtn {
+    padding: 3px 16px;
+  }
+`;
+const HeaderTopRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const DaysOfWeekWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  border-bottom: 1px solid #404040;
+`;
+const DayOfWeek = styled.h2<{ $weekendDay?: boolean }>`
+  display: flex;
+  justify-content: flex-end;
+  font-weight: 500;
+  margin-right: 10px;
 
+  color: ${(props) => (props.$weekendDay ? '#7e7e7e' : 'white')};
+`;
+const DatesWrapper = styled.section`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(7, 1fr);
+  background-color: #404040;
+  grid-gap: 1px;
+`;
 export default CalendarComponent;
--- src/components/Calendar/DateItem.tsx
+++ src/components/Calendar/DateItem.tsx
@@ -3,20 +3,56 @@ import React, { FC } from 'react';
 import { styled } from 'styled-components';
 interface DateObj {
   date: string;
+  thisMonth: string;
 }
-const DateItem: FC<DateObj> = ({ date }) => {
+const DateItem: FC<DateObj> = ({ date, thisMonth }) => {
   return (
-    <Wrapper>
-      {date.split(' ')[0] === '1'
-        ? date.split(' ').join('. ')
-        : date.split(' ')[0]}
+    <Wrapper
+      $isThisMonth={date.split(' ')[1] === thisMonth.substring(0, 3)}
+      $isToday={date === moment().format('D MMM Y ddd')}
+      $isWeekend={date.split(' ')[3] === 'Sun' || date.split(' ')[3] === 'Sat'}
+    >
+      {date.split(' ')[0] === '1' ? (
+        <>
+          <p>{date.split(' ')[0]}</p> <h3>{date.split(' ')[1]}</h3>
+        </>
+      ) : (
+        <p>{date.split(' ')[0]}</p>
+      )}
     </Wrapper>
   );
 };
-const Wrapper = styled.div`
+const Wrapper = styled.div<{
+  $isToday?: boolean;
+  $isThisMonth?: boolean;
+  $isWeekend?: boolean;
+}>`
   display: flex;
   background-color: #1f1e1e;
-  color: white;
+  P {
+    margin: 5px 3px;
+    padding: 3px;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    text-align: center;
+
+    color: ${(props) => (props.$isWeekend ? '#818181' : 'white')};
+
+    color: ${(props) => (props.$isThisMonth ? '' : '#3d3d3d')};
+    color: ${(props) => (props.$isToday ? 'black' : '')};
+
+    font-size: 17px;
+    background-color: ${(props) => (props.$isToday ? '#eb2525' : '#1f1e1e')};
+  }
+  h3 {
+    font-weight: 400;
+    padding: 6px 0;
+    color: ${(props) => (props.$isWeekend ? '#818181' : 'white')};
+
+    color: ${(props) => (props.$isThisMonth ? '' : '#505050')};
+    margin-right: 10px;
+  }
   justify-content: flex-end;
   min-height: 100px;
   min-width: 140px;
